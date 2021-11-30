a63981471d45a5e972f09768f8761e8e6f48741f
Alexandr12121 <91566444+Alexandr12121@users.noreply.github.com>
2021-11-30 18:52:58 +0300

    Delete laba_pandas directory

--- laba_pandas/task1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-arr = pd.read_csv("transactions.csv")
-Real = arr[arr['STATUS'] == 'OK']
-print("Three biggest: \n", Real.loc[Real['SUM'].nlargest(3).index])
-print("Summa of Umbrella transactions: \n", Real.loc[Real['CONTRACTOR'] == 'Umbrella, Inc']['SUM'].sum())
